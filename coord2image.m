2eb21bd8c0628db0904aba0324d2ac2022c56860
yehmon01 <CHJoshuaYeh@gmail.com>
2018-02-21 17:19:14 +0100

    Added a new fcn for converting coordinates to image array and a small update to import_tiff_stacks

---
+++ coord2image.m
@@ -0,0 +1,48 @@
+function [out,A,B]=coord2image(X,Y,Z1,w,type)
+% Author: Joshua Yeh
+% Date created: 2018-21-2
+%
+% DESCRIPTION
+% This script converts xyz coordinates into an image array.
+% 
+%% INPUT VARIABLES
+% X: x coordinates
+% 
+% Y: y coordinates
+% 
+% Z1: z coordinates
+% 
+% w: the bin width or size of each pixel in the image array
+% 
+% type:
+    % 'mean': take the mean of the datapoints within the same bin
+    % 'none': uses the default behavior of summing all of the datapoints
+% 
+%% OUTPUT VARIABLES
+% out: the image array
+% 
+% A: x meshgrid variable associated with the image array
+% 
+% B: y meshgrid variable associated with the image array
+% 
+%%
+
+% This fcns converts coordinates of intensities into an image array
+n1=min(X):w:max(X);
+n2=min(Y):w:max(Y);
+a1=linspace(min(X),max(X),numel(n1));
+b1=linspace(min(Y),max(Y),numel(n2));
+
+% Perform the binning procedure
+ar=interp1(a1,1:numel(a1),X(:),'nearest');
+br=interp1(b1,1:numel(b1),Y(:),'nearest');
+
+% Convert coordinates into an image array
+if strcmp(type,'mean')
+    out=accumarray([br ar],Z1(:),[numel(n2) numel(n1)],@(x) mean(x));
+elseif strcmp(type,'none')
+    out=accumarray([br ar],Z1(:),[numel(n2) numel(n1)]);
+end
+
+% Create corresponding X and Y array associated withthe image array
+[A,B]=meshgrid(a1,b1);
--- import_tiff_stack.m
+++ import_tiff_stack.m
@@ -1,4 +1,4 @@
-function output=import_tiff_stack(file,varargin)
+function output=import_tiff_stack(file,r,varargin)
 %% DESCRIPTION
 % This function imports a stacked tiff file and stores in an ouput variable
 % structure. This function only supports greyscale or binary images!
@@ -6,44 +6,59 @@ function output=import_tiff_stack(file,varargin)
 %% INPUT VARIABLES
 % file: names of the tiff file to be imported
 % 
-% r (varargin{1}): the pixe side length of the square area in which a
+% r: the pixe side length of the square area in which a
 % medfilt2 will be applied (default is r=10)
+% 
+% varargin 'fieldname',<value>
+        % 'skip': skip basic calculations of images
+        % 'silence': turn off some commands that display during importing
 % 
 %% OUTPUT VARIABLES
 % output: structure variable containing the raw image file and integrated
 %   intensity through the thickness of the stack
 % 
 %%
-switch nargin
-    case 1
-        r=10;
-    case 2
-        r=varargin{1};
-end
+% parse user inputs
+narginchk(2,inf);
+params=inputParser;
+params.addParameter('skip',0,@(x) isnumeric(x)&(x==1||x==0));
+params.addParameter('silence',0,@(x) isnumeric(x)&(x==1||x==0));
+params.parse(varargin{:});
 
-disp(['Importing ',file]);
+skip=params.Results.skip;
+silence=params.Results.silence;
+
+% parse file variable
+[~,name,ext]=fileparts(file);
+
+disp(['Importing ',name,ext]);
 tiff_info=imfinfo(file);%tiff info
-disp(['Detected ',num2str(size(tiff_info,1)),' page(s) in tiff file']);
+disp(['Detected ',num2str(size(tiff_info,1)),' page(s) in tif file']);
 tiff_stack=imread(file,1);%read first image in the stack
 tiff_stack=medfilt2(double(tiff_stack),[r r]);% remove noise using medium filter
-I_sum_z=sum(tiff_stack(:));% integrate intensity for first image plane
-tiff_stack_std=std(tiff_stack(:));%determine std for each plane
-tiff_stack_mean=mean(tiff_stack(:));%determine mean for each plane
-tiff_stack_median=median(tiff_stack(:));%determine median for each plane
-tiff_stack_q1=quantile(tiff_stack(:),0.25);%determine 1st quartile
-tiff_stack_q3=quantile(tiff_stack(:),0.75);%determine 3rd quartile
+
+if skip==0
+    I_sum_z=sum(tiff_stack(:));% integrate intensity for first image plane
+    tiff_stack_std=std(tiff_stack(:));%determine std for each plane
+    tiff_stack_mean=mean(tiff_stack(:));%determine mean for each plane
+    tiff_stack_median=median(tiff_stack(:));%determine median for each plane
+    tiff_stack_q1=quantile(tiff_stack(:),0.25);%determine 1st quartile
+    tiff_stack_q3=quantile(tiff_stack(:),0.75);%determine 3rd quartile
+end
 
 %extract and apply median filter to each image in stack and concatenate
 for dum=2:size(tiff_info,1)
     plane=double(medfilt2(imread(file,dum),[r r]));
     tiff_stack=cat(3,tiff_stack,plane);
-    I_sum_z=cat(1,I_sum_z,sum(plane(:)));%integrate intensity for image plane
-    tiff_stack_std=cat(1,tiff_stack_std,std(plane(:)));%det. std for each plane
-    tiff_stack_mean=cat(1,tiff_stack_mean,mean(plane(:)));%det. mean for each plane
-    tiff_stack_median=cat(1,tiff_stack_median,median(tiff_stack(:)));
-    tiff_stack_q1=cat(1,tiff_stack_q1,quantile(tiff_stack(:),0.25));
-    tiff_stack_q3=cat(1,tiff_stack_q3,quantile(tiff_stack(:),0.75));
-    if mod(dum,ceil(size(tiff_info,1)*0.25))==0
+    if skip==0
+        I_sum_z=cat(1,I_sum_z,sum(plane(:)));%integrate intensity for image plane
+        tiff_stack_std=cat(1,tiff_stack_std,std(plane(:)));%det. std for each plane
+        tiff_stack_mean=cat(1,tiff_stack_mean,mean(plane(:)));%det. mean for each plane
+        tiff_stack_median=cat(1,tiff_stack_median,median(tiff_stack(:)));
+        tiff_stack_q1=cat(1,tiff_stack_q1,quantile(tiff_stack(:),0.25));
+        tiff_stack_q3=cat(1,tiff_stack_q3,quantile(tiff_stack(:),0.75));
+    end
+    if mod(dum,ceil(size(tiff_info,1)*0.25))==0&&silence==0
         disp([num2str(dum),' of ',num2str(size(tiff_info,1)),' imported...']);
     end
 end
@@ -52,13 +67,15 @@ tiff_stack_sum=sum(tiff_stack,3);%determine sum of each plane
 
 %Store variables in the output variable structure
 output.tiff_stack=tiff_stack;%filtered (via medfilt2) 3d image
-output.I_sum_z=I_sum_z;%total intensity for each plane
-output.tiff_stack_sum=tiff_stack_sum;%summed intensity through thickness
-output.tiff_stack_mean=tiff_stack_mean;%mean of each plane
-output.tiff_stack_median=tiff_stack_median;%median of each plane
-output.tiff_stack_q1=tiff_stack_q1;%1st quartile of each plane
-output.tiff_stack_q3=tiff_stack_q3;%1st quartile of each plane
-output.tiff_stack_std=tiff_stack_std;%std of each plane
+if skip==0
+    output.I_sum_z=I_sum_z;%total intensity for each plane
+    output.tiff_stack_sum=tiff_stack_sum;%summed intensity through thickness
+    output.tiff_stack_mean=tiff_stack_mean;%mean of each plane
+    output.tiff_stack_median=tiff_stack_median;%median of each plane
+    output.tiff_stack_q1=tiff_stack_q1;%1st quartile of each plane
+    output.tiff_stack_q3=tiff_stack_q3;%1st quartile of each plane
+    output.tiff_stack_std=tiff_stack_std;%std of each plane
+end
 output.file=file;%filename of the imported tiff file
 output.info=imfinfo(file);%Stores the metadata information
 disp('Import finished');
